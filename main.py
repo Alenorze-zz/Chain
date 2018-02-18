2981a087afc1cd94657ffc9bcc9070f13e663bd6
Алексей Белов <Alenorze@gmail.com>
2018-02-18 10:36:07 +0200

    Form Redirect

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from block import *
 
 app = Flask(__name__)
@@ -10,6 +10,10 @@ def index():
         lender = request.form['lender']
         amount = request.form['amount']
         borrower = request.form['borrower']
+
+        write_block(name=lender, amount=amount, to_whom=borrower)
+        return redirect(url_for("index"))
+
     return render_template('index.html')
 
 
