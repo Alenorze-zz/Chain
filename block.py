89e594a5b314d98e18d932d71cce0b989f5286e2
Алексей Белов <Alenorze@gmail.com>
2018-02-18 07:33:06 +0200

    List Sorting

--- block.py
+++ block.py
@@ -1,18 +1,27 @@
+import os
 import json
 
 
 
 def write_block(name, amount, to_whom, hash=''):
-    data = {'name': 'Klossy',
-            'amount': '22',
-            'to_whom': 'Me',
-            'hash': '123'}
-    with open('test', 'w') as file:
+    
+    blockchain_dir = os.curdir + '/blockchain/' #./blockchain/test
+
+    files = os.listdir(blockchain_dir)
+    files = sorted([int(i) for i in files])
+
+    last_file = files[-1]
+
+    data = {'name': name,
+            'amount': amount,
+            'to_whom': to_whom,
+            'hash': prev_hash}
+    with open(blockchain_dir + 'test', 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    write_block()
+    write_block(name='Klossy', amount='22', to_whom='Me')
 
 if __name__ == '__main__':
     main()
