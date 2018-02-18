2baf21b99667fee37f84599176debfee61ab4c22
Алексей Белов <Alenorze@gmail.com>
2018-02-18 07:43:31 +0200

    Block Hashing

--- block.py
+++ block.py
@@ -1,22 +1,31 @@
 import os
 import json
+import hashlib
 
 
+
+def get_hash(filename):
+    blockchain_dir = os.curdir + '/blockchain/'
+    file = open(blockchain_dir + filename, 'rb').read()
+    return hashlib.md5(file).hexdigest()
 
 def write_block(name, amount, to_whom, hash=''):
-    
     blockchain_dir = os.curdir + '/blockchain/' #./blockchain/test
 
     files = os.listdir(blockchain_dir)
     files = sorted([int(i) for i in files])
 
     last_file = files[-1]
+    filename = str(last_file + 1)
+
+    prev_hash = get_hash(str(last_file))
 
     data = {'name': name,
             'amount': amount,
             'to_whom': to_whom,
             'hash': prev_hash}
-    with open(blockchain_dir + 'test', 'w') as file:
+
+    with open(blockchain_dir + filename, 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
